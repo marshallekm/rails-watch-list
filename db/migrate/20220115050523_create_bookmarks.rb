094b3e06ece389da568d11ee164f293affa5ef28
Erika Kawashima <marshallekm@gmail.com>
2022-01-15 14:08:38 +0900

    finished model and dependences

--- db/migrate/20220115050523_create_bookmarks.rb
+++ db/migrate/20220115050523_create_bookmarks.rb
@@ -1,7 +1,7 @@
 class CreateBookmarks < ActiveRecord::Migration[6.1]
   def change
     create_table :bookmarks do |t|
-      t.text :comment
+      t.string :comment
       t.references :movie, null: false, foreign_key: true
       t.references :list, null: false, foreign_key: true
 
