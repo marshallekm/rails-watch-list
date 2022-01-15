840b0228dedfe18345b45c32ea6b3b7a0d1295d7
Erika Kawashima <marshallekm@gmail.com>
2022-01-15 17:04:17 +0900

    up to bookmark controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :new, :show, :create]
+  root to:"lists#index"
+  resources :lists, only: [:index, :new, :show, :create] do
+    resources :bookmarks, only: [:create, :new]
+  end
+  resources :bookmarks, only: [:destroy]
 end
