cdb22b6ff2db4ffc2b50a0ac147c01c227ebbc71
Erika Kawashima <marshallekm@gmail.com>
2022-01-15 15:05:30 +0900

    added list routes

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks
+  #bookmarks come first because it is connected to the movies joint table
   has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, presence: true, uniqueness: true
 end
