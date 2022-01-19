ba398c6e09b7aa61d7fe339bd31956cbeacbc860
Erika Kawashima <marshallekm@gmail.com>
2022-01-19 13:02:19 +0900

    fixed nav links

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks
+  has_one_attached :photo
   #bookmarks come first because it is connected to the movies joint table
   has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, presence: true, uniqueness: true
